b57394734a25c13809ea7cbbd2c1abce805fe88e
geana.a <andreigeana71@gmail.com>
2025-04-01 21:59:51 +0300

    SSP-18 <Initialize auth controller>
    Add auth controller.
    Add documentation to auth controller.
    Add Swagger documentation generation.
    Delete bloat data from project.

---
+++ backend/Catalogo-Escolar-API/Catalogo-Escolar-API/Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalogo_Escolar_API.Controllers
+{
+    /// <summary>
+    /// Represents the authentication controller. It is used to authenticate users and any actions it may include, such as JWT creation.
+    /// </summary>
+    [ApiController]
+    [Route("auth")]
+    public class AuthController : ControllerBase
+    {
+        /// <summary>
+        /// Represents the default constructor of the <see cref="AuthController"/> class.
+        /// </summary>
+        public AuthController() { }
+
+        /// <summary>
+        /// Authenticates the user and generates a JWT token.
+        /// </summary>
+        /// <returns>A JWT token if authentication is successful.</returns>
+        /// <response code="200">Authentication successful.</response>
+        /// <response code="400">Error.</response>
+        [HttpPost("login")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult<string>> Login()
+        {
+            try
+            {
+                return Ok("This is your JWT");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Failed to log in user.");
+            }
+        }
+
+        /// <summary>
+        /// Registers a new user.
+        /// </summary>
+        /// <returns>A boolean indicating whether the registration was successful.</returns>
+        /// <response code="200">Registration successful.</response>
+        /// <response code="400">Error.</response>
+        [HttpPost("register")]
+        public async Task<ActionResult<bool>> Register()
+        {
+            try
+            {
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Failed to register user.");
+            }
+        }
+    }
+}
--- backend/Catalogo-Escolar-API/Catalogo-Escolar-API/Program.cs
+++ backend/Catalogo-Escolar-API/Catalogo-Escolar-API/Program.cs
@@ -1,11 +1,22 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Add controllers to the container
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    //Add custom controller summaries
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
