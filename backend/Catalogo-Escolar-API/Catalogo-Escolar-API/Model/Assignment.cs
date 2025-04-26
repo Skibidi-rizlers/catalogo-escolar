bb869e2360a37108eca18587c82e190d7d38177e
geana.a <andreigeana71@gmail.com>
2025-04-26 19:36:56 +0300

    SSP-23 <Add migration>

---
+++ backend/Catalogo-Escolar-API/Catalogo-Escolar-API/Model/Assignment.cs
@@ -0,0 +1,16 @@
+﻿namespace Catalogo_Escolar_API.Model
+{
+    public class Assignment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public int ClassId { get; set; }
+        public Class Class { get; set; }
+
+        public ICollection<Grade> Grades { get; set; }
+    }
+
+}
